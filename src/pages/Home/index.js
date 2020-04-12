87cf131c16aae4c98d494aaa6a3d5f318d911ea6
Luiz Paulo de Oliveira <luiz.paulo.oliveira@vli-logistica.com.br>
2020-04-11 22:10:15 -0300

    Mapeando conexões

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,17 @@
 import Home from './Home'
 import { connect } from 'react-redux'
 
-import { getListTotal } from '../../store/reducers/list'
+import {
+  getListTotal,
+  getOpenedItems,
+  getClosedItems
+} from '../../store/reducers/list'
 
 const mapStateToProps = state => ({
   list: state.list,
-  total: getListTotal(state)
+  total: getListTotal(state),
+  openedItems: getOpenedItems(state),
+  closedItems: getClosedItems(state)
 })
 
 export default connect(mapStateToProps, null)(Home)
--- src/store/reducers/list.js
+++ src/store/reducers/list.js
@@ -41,6 +41,7 @@ export default function list (state = initialState, action) {
   }
 }
 
+// Helpers
 function getItemTotal (product) {
   return product.price * product.quantity
 }
@@ -55,7 +56,18 @@ function toggleItem (items, productId) {
   ]
 }
 
+// Selectors
 export const getListTotal = createSelector(
   state => state.list.items,
   items => items.reduce((total, item) => total + item.total, 0)
+)
+
+export const getOpenedItems = createSelector(
+  state => state.list.items,
+  items => items.filter(item => !item.checked).length
+)
+
+export const getClosedItems = createSelector(
+  state => state.list.items,
+  items => items.filter(item => item.checked).length
 )
